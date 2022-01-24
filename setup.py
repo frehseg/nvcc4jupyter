46a87bc6dbf7ad96771a1ff3fce021c3c03d2a55
frehseg <30627181+frehseg@users.noreply.github.com>
2022-01-24 12:18:04 +0100

    Update setup.py
    Increased version number

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 
 setup(
     name='NVCCPlugin',
-    version='0.0.1',
-    author='Andrei Nechaev',
-    author_email='lyfaradey@yahoo.com',
+    version='0.2',
+    author='Andrei Nechaev / Goran Frehse',
+    author_email='',
     py_modules=['nvcc_plugin'],
-    url='htpps://github.com/andreinechaev/nvcc4jupyter',
+    url='htpps://github.com/frehseg/nvcc4jupyter',
     license='LICENSE',
     description='Jupyter notebook plugin to run CUDA C/C++ code',
-    # long_description=open('README.md').read(),
+    long_description=open('README.md').read(),
 )
